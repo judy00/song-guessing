53a1fc725ee640e8d45481d73c17384cd60a38d6
JudyWu <judywu@sat.cool>
2023-12-08 17:41:15 +0800

    feat(song): add song page

--- pages/category/[id].vue
+++ pages/category/[id].vue
@@ -1,20 +1,40 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const route = useRoute()
+
+console.log(route)
+</script>
 
 <template>
   <div class="bg-gray-400">
-    <NuxtLink :to="{ name: 'index', params: { id: 1 } }">back {{ $route.params.id }}</NuxtLink>
-
-    <div class="container flex justify-center items-center mx-auto py-8 px-40 min-h-screen">
+    <div v-if="$route.name === 'category-id'" class="container relative flex justify-center items-center mx-auto py-8 px-40 min-h-screen">
+      <NuxtLink class="flex items-center mr-8 bg-white/80 p-4 rounded-full shadow shadow-xl shadow-teal-500/40 hover:shadow-teal-500/30 hover:bg-white/100 duration-300" :to="{ name: 'index' }">
+        <svg class="w-6 h-6 text-teal-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
+        </svg>
+      </NuxtLink>
       <div class="grid gap-x-8 gap-y-10 flex-1">
         <h1 class="mb-8 text-8xl font-extrabold text-center text-gray-900 dark:text-white">
           <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-200 from-sky-400">多益</span>職涯力
         </h1>
-        <NuxtLink class="song-btn" :to="{ name: 'category-id', params: { id: 1 } }">⭐ 西洋夜店勁曲 (2010)</NuxtLink>
-        <NuxtLink class="song-btn" :to="{ name: 'category-id', params: { id: 2 } }">⭐⭐ 超級變變變 (2003)</NuxtLink>
-        <NuxtLink class="song-btn" :to="{ name: 'category-id', params: { id: 3 } }">⭐ 女力駕到 (2000)</NuxtLink>
-        <NuxtLink class="song-btn" :to="{ name: 'category-id', params: { id: 4 } }">⭐⭐⭐ 科技執法中 (2013)</NuxtLink>
+        <NuxtLink class="song-btn"
+          :to="{ name: 'category-id-song-songId', params: { id: $route.params.id, songId: 1 } }">
+          ⭐ 西洋夜店勁曲 (2010)
+        </NuxtLink>
+        <NuxtLink class="song-btn"
+          :to="{ name: 'category-id-song-songId', params: { id: $route.params.id, songId: 2 } }">
+          ⭐⭐ 超級變變變 (2003)
+        </NuxtLink>
+        <NuxtLink class="song-btn"
+          :to="{ name: 'category-id-song-songId', params: { id: $route.params.id, songId: 3 } }">
+          ⭐ 女力駕到 (2000)
+        </NuxtLink>
+        <NuxtLink class="song-btn"
+          :to="{ name: 'category-id-song-songId', params: { id: $route.params.id, songId: 5 } }">
+          ⭐⭐⭐ 科技執法中 (2013)
+        </NuxtLink>
       </div>
     </div>
+    <NuxtPage></NuxtPage>
   </div>
 </template>
 
---
+++ pages/category/[id]/song/[songId].vue
@@ -0,0 +1,69 @@
+<script lang="ts" setup>
+const route = useRoute()
+const step = ref(1)
+
+const question = ref(`孔子的中心思想是個仁 仁的表現是？
+★★★★★★  ★★★★★★  ★★★★  ★★★★★`)
+
+const answer = ref(`孔子的中心思想是個仁 仁的表現是？
+己欲立而立人 己欲達而達人 己所不欲 他勿施於人`)
+
+const onClickNext = () => {
+  if (step.value === 1) {
+    step.value++
+    return
+  }
+
+  if (step.value === 2) {
+    // isAnswered.value = true
+    navigateTo({
+      name: 'category-id',
+      params: { id: route.params.id }
+    })
+  }
+}
+</script>
+
+<template>
+  <div class="bg-gray-400">
+    <div class="container relative flex flex-col justify-around items-center mx-auto py-8 min-h-screen">
+      <h1 class="song-title text-8xl font-extrabold text-center text-white">
+        《論語》(1000)
+      </h1>
+
+      <h2 v-show="step === 1" class="text-zinc-900 text-6xl text-center font-semibold leading-snug tracking-wide whitespace-pre">
+        {{ question }}
+      </h2>
+
+      <h2 v-show="step === 2" class="text-zinc-900 text-6xl text-center font-semibold leading-snug tracking-wide whitespace-pre">
+        {{ answer }}
+      </h2>
+
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 37" class="song-arrow w-4" @click="onClickNext">
+        <path fill="#5D5F60" fill-rule="evenodd" d="M.293.293a1 1 0 0 1 1.414 0L6 4.586 10.293.293a1 1 0 1 1 1.414 1.414l-5 5a1 1 0 0 1-1.414 0l-5-5a1 1 0 0 1 0-1.414Z" clip-rule="evenodd" class="arrow"></path>
+        <path fill="#5D5F60" fill-rule="evenodd" d="M.293 15.293a1 1 0 0 1 1.414 0L6 19.586l4.293-4.293a1 1 0 0 1 1.414 1.414l-5 5a1 1 0 0 1-1.414 0l-5-5a1 1 0 0 1 0-1.414Z" clip-rule="evenodd" class="arrow"></path>
+        <path fill="#5D5F60" fill-rule="evenodd" d="M.293 30.293a1 1 0 0 1 1.414 0L6 34.586l4.293-4.293a1 1 0 0 1 1.414 1.414l-5 5a1 1 0 0 1-1.414 0l-5-5a1 1 0 0 1 0-1.414Z" clip-rule="evenodd" class="arrow"></path>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<style>
+.song-title {
+  text-shadow: #61c4bf 4px 4px 10px;
+}
+
+.song-arrow {
+  cursor: pointer;
+  animation: floatAnimation 2s infinite ease-in-out;
+}
+
+@keyframes floatAnimation {
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-10px);
+  }
+}
+</style>
