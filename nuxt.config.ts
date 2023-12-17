893fec0a82d3077019001ac8361da83f8ba3654f
JudyWu <judywu@sat.cool>
2023-12-18 02:15:19 +0800

    refactor(deploy): modify nojekyll file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt'
-  ],
-  css: ['~/assets/main.css'],
   app: {
     head: {
       title: '塞特百萬大歌星'
     },
     baseURL: process.env.NODE_ENV === 'production' ? '/song-guessing/' : '/',
     buildAssetsDir: '/static/'
-  }
+  },
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt'
+  ],
+  css: ['~/assets/main.css']
 })
