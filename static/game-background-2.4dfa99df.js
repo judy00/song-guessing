0d54c17704dc5641f25f9fbd3b39d4ce43627dc1
JudyWu <judywu@sat.cool>
2024-12-19 23:14:54 +0800

    Updates

---
+++ static/game-background-2.4dfa99df.js
@@ -0,0 +1,1 @@
+import"./entry.d8a82c8a.js";const m=""+new URL("game-background-2.cb898b6c.mp4",import.meta.url).href;export{m as _};
---
+++ static/game-background-3.fd25c35a.js
@@ -0,0 +1,1 @@
+import"./entry.d8a82c8a.js";const e=""+new URL("game-background-3.a9d1e33d.mp4",import.meta.url).href;export{e as _};
